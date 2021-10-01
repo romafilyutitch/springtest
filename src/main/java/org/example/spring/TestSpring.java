6bfd19a105675486c3a66a95534eb571e9c8dda7
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-10-01 16:20:19 +0300

    bean init, destroy, factory methods

--- src/main/java/org/example/spring/TestSpring.java
+++ src/main/java/org/example/spring/TestSpring.java
@@ -5,15 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        boolean comparison = musicPlayer1 == musicPlayer2;
-        System.out.println(comparison);
-        System.out.println(musicPlayer1);
-        System.out.println(musicPlayer2);
-        musicPlayer1.setVolume(10);
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+        ClassicalMusic bean = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(bean.getSong());
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        boolean comparison = musicPlayer1 == musicPlayer2;
+//        System.out.println(comparison);
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
+//        musicPlayer1.setVolume(10);
+//        System.out.println(musicPlayer1.getVolume());
+//        System.out.println(musicPlayer2.getVolume());
 //        System.out.println(musicPlayer1.getName());
 //        System.out.println(musicPlayer1.getVolume());
         context.close();
