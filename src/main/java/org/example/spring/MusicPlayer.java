aa1099497b03cfbfa16c1b2bc1136bd924e99c51
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-10-01 15:59:41 +0300

    Bean scope basics

--- src/main/java/org/example/spring/MusicPlayer.java
+++ src/main/java/org/example/spring/MusicPlayer.java
@@ -2,13 +2,30 @@ package org.example.spring;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
-    //IoC
-    public MusicPlayer(Music music) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing music: " + music.getSong());
     }
 }
--- src/main/java/org/example/spring/TestSpring.java
+++ src/main/java/org/example/spring/TestSpring.java
@@ -5,8 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        boolean comparison = musicPlayer1 == musicPlayer2;
+        System.out.println(comparison);
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+        musicPlayer1.setVolume(10);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
+//        System.out.println(musicPlayer1.getName());
+//        System.out.println(musicPlayer1.getVolume());
         context.close();
     }
 }
