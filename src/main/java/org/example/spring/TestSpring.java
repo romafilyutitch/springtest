b2d88dd576d9ec7543a83103bb4af13a4c1991c6
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-10-01 16:51:56 +0300

    Autowied annotation use

--- src/main/java/org/example/spring/TestSpring.java
+++ src/main/java/org/example/spring/TestSpring.java
@@ -1,16 +1,13 @@
 package org.example.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("rockMusic", Music.class);
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer classicalPlayer = new MusicPlayer(music2);
-        MusicPlayer player = new MusicPlayer(music);
-        player.playMusic();
-        classicalPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
